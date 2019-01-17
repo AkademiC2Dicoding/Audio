b80d56eb43bacca79b006341e860cc20e59d43c7
Unknown <punto.damar2@gmail.com>
2019-01-17 10:55:00 +0700

    export

---
+++ docs/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1547697194,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/playerbullet-sheet0.png",
+		"media/kena_tembak.ogg",
+		"media/shotgun.ogg",
+		"media/menu.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
